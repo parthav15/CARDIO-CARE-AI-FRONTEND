713d7cb222fa71322d011929428d362c5aa1bd8b
parthav15 <parthavsabrwal@gmail.com>
2025-03-02 18:48:28 +0530

    Updated config.jsx

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,4 +1,3 @@
-export const BASE_URL = 'http://localhost:8000/';
+// export const BASE_URL = 'http://localhost:8000/';
 
-// export const BASE_URL = 'https://crimeradarai.pythonanywhere.com/';
-
+export const BASE_URL = 'http://3.86.38.245:8000/';
